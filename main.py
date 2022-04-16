e5951b77d572fd44819ef87baee596ddce29089c
Diwash Subedi <diwashsubedi@outlook.com>
2022-04-16 12:27:49 -0400

    file name change

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
 import cv2
 import numpy as np
 import face_recognition
+import os
+from attendance import markAttendance
 
 
-def mainfun():
-    # Image import
-    imgDiwas = face_recognition.load_image_file('ImageMain/Diwash.jpg')
-    imgDiwas = cv2.cvtColor(imgDiwas, cv2.COLOR_BGR2RGB)
-    imgTest = face_recognition.load_image_file('ImageMain/Bobby.jpg')
-    imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+def name_attendance(images,classNames):
+    # find encoding images
+    def findEncodings(images):
+        encodeList = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
+    encodeListKnown = findEncodings(images)
+    cap = cv2.VideoCapture(0)
 
-    # Face Detect
-    faceLoc = face_recognition.face_locations(imgDiwas)[0]
-    encodeDiwash = face_recognition.face_encodings(imgDiwas)[0]
-    cv2.rectangle(imgDiwas, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+    while True:
+        # reading data from webcam
+        success, img = cap.read()
+        # image resize
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        # convert into RGB from BGR
+        imgS = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-    # Face Detect
-    faceLocTest = face_recognition.face_locations(imgTest)[0]
-    encodetest = face_recognition.face_encodings(imgTest)[0]
-    cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
+        # finding the location of the faces in webcam frame
+        facesCurFrame = face_recognition.face_locations(imgS)
+        # finding the encoding of the webcam
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    # Comparing the distance and finding the spaces
-    result = face_recognition.compare_faces([encodeDiwash], encodetest)
-    faceDis = face_recognition.face_distance([encodeDiwash], encodetest)
-    print(result, faceDis)
-    cv2.putText(imgTest, f'{result}{round(faceDis[0], 2)}', (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+        # One by One it graph one faceLoc from facesCurfame list and encoding face of the encodesCurFrame
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            # print(faceDis)
+            matchIndex = np.argmin(faceDis)
 
-    # Image Show
-    cv2.imshow('Diwash Subedi', imgDiwas)
-    cv2.imshow('Diwash Test', imgTest)
-    cv2.waitKey(0)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                # print(name)
+                y1, x2, y2, x1 = faceLoc
+                # y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markAttendance(name)
+
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
 
 
 if __name__ == '__main__':
-    mainfun()
+    path = 'ImageAttendance'
+    imges = []
+    classNames = []
+    myList = os.listdir(path)
+    for cl in myList:
+        crtImg = cv2.imread(f'{path}/{cl}')
+        imges.append(crtImg)
+        classNames.append(os.path.splitext(cl)[0])
+    name_attendance(imges,classNames)
